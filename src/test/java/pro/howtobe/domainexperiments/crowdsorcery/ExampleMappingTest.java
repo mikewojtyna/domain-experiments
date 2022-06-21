b03f7662e78390bfe731fde2b52a7a2b7a6a45ec
Mike Wojtyna <mike@buildmysoftware.pro>
2022-06-22 00:39:53 +0200

    Starting a project: Step 2

--- src/test/java/pro/howtobe/domainexperiments/crowdsorcery/ExampleMappingTest.java
+++ src/test/java/pro/howtobe/domainexperiments/crowdsorcery/ExampleMappingTest.java
@@ -3,6 +3,8 @@ package pro.howtobe.domainexperiments.crowdsorcery;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,16 +41,17 @@ class ExampleMappingTest {
         // @formatter:off
         @DisplayName(
             """
-             given borrower who is 17yo,
+             given borrower who is younger than 18 years old,
              when borrower wants to start a project,
              then project is not started
             """
         )
         // @formatter:on
-        @Test
-        void seventeenYoTest() {
+        @ParameterizedTest(name = "given borrower is {0} years old.")
+        @ValueSource(ints = {17, 16, 8, 4, 2, 1})
+        void tooYoungTest(int years) {
             // given
-            borrower = borrowerOfAge(17);
+            borrower = borrowerOfAge(years);
 
             // when
             var events = borrowerWantsToStartAProject();
