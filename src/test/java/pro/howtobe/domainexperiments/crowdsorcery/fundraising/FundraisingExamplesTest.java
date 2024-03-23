c3a5cc0a6f269dbdfb89d5f6034968636fd6674c
Mike Wojtyna <mike@wojtyna.pl>
2024-03-23 23:13:02 +0100

    Prepare examples stub

---
+++ src/test/java/pro/howtobe/domainexperiments/crowdsorcery/fundraising/FundraisingExamplesTest.java
@@ -0,0 +1,8 @@
+package pro.howtobe.domainexperiments.crowdsorcery.fundraising;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Fundraising examples taken from Miro example mapping session https://miro.com/app/board/uXjVNhVgRkU=/")
+class FundraisingExamplesTest {
+
+}
