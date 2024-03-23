71c1608d69d536c53bbc0c111a591f34d52984a8
Mike Wojtyna <mike@wojtyna.pl>
2024-03-23 23:17:56 +0100

    Prepare draft of "project is funded" example

---
+++ src/main/java/pro/howtobe/domainexperiments/crowdsorcery/fundraising/SomeSystemIDontKnowHowToNameYet.java
@@ -0,0 +1,8 @@
+package pro.howtobe.domainexperiments.crowdsorcery.fundraising;
+
+public class SomeSystemIDontKnowHowToNameYet {
+
+    public ProjectIsFunded fundProject() {
+        return new ProjectIsFunded();
+    }
+}
--- src/test/java/pro/howtobe/domainexperiments/crowdsorcery/fundraising/FundraisingExamplesTest.java
+++ src/test/java/pro/howtobe/domainexperiments/crowdsorcery/fundraising/FundraisingExamplesTest.java
@@ -1,8 +1,29 @@
 package pro.howtobe.domainexperiments.crowdsorcery.fundraising;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Fundraising examples taken from Miro example mapping session https://miro.com/app/board/uXjVNhVgRkU=/")
 class FundraisingExamplesTest {
 
+    // @formatter:off
+    @DisplayName(
+        """
+         project is funded
+        """
+    )
+    // @formatter:on
+    @Test
+    void projectIsFundedTest() {
+        // given
+        var someSystemIDontKnowHowToNameYet = new SomeSystemIDontKnowHowToNameYet();
+
+        // when
+        var projectIsFunded = someSystemIDontKnowHowToNameYet.fundProject();
+
+        // then
+        assertThat(projectIsFunded).isNotNull();
+    }
 }
